c96ba74e200b95bd1b16c2c193cc24df81803e92
merunga <merunga@gmail.com>
2022-03-16 01:06:04 -0500

    agregar tests

---
+++ test/lib.test.js
@@ -0,0 +1,19 @@
+import { assertEquals } from "https://deno.land/std@0.129.0/testing/asserts.ts";
+
+import * as cipher from '../src/lib.js';
+
+Deno.test('encode: should return "HIJKLMNOPQRSTUVWXYZABCDEFG" for "ABCDEFGHIJKLMNOPQRSTUVWXYZ" with offset 33', () => {
+  assertEquals(cipher.encode(33, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'),'HIJKLMNOPQRSTUVWXYZABCDEFG');
+});
+
+Deno.test('encode: should return "hijklmnopqrstuvwxyzabcdefg" for "abcdefghijklmnopqrstuvwxyz" with offset 33', () => {
+  assertEquals(cipher.encode(33, 'abcdefghijklmnopqrstuvwxyz'), 'hijklmnopqrstuvwxyzabcdefg');
+});
+
+Deno.test('decode: should return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" for "HIJKLMNOPQRSTUVWXYZABCDEFG" with offset 33', () => {
+  assertEquals(cipher.decode(33, 'HIJKLMNOPQRSTUVWXYZABCDEFG'), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+});
+
+Deno.test('decode: should return "abcdefghijklmnopqrstuvwxyz" for "hijklmnopqrstuvwxyzabcdefg" with offset 33', () => {
+  assertEquals(cipher.decode(33, 'hijklmnopqrstuvwxyzabcdefg'), 'abcdefghijklmnopqrstuvwxyz');
+});
